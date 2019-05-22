224fba183b5e3bd55d6a40d917b61333926fa8e0
Ryan Lau <rlau1185@stu.d214.org>
2019-05-22 14:44:21 -0500

    everything

--- CalculatorUnitApp/ViewController.swift
+++ CalculatorUnitApp/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var gradeNeeded: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func myButton(_ sender: Any) {
+        let gradeC = Double(textField1.text!)!
+        let gradeW = Double(textField2.text!)!
+        let weight = Double(textField3.text!)!
+        let decimalPercent = weight / 100.0
+        let percentLeft = 1 - decimalPercent
+        let gradeN = (gradeW - (percentLeft * gradeC)) / decimalPercent
+        gradeNeeded.text = "\(gradeN)"
+    }
+    }
 
-
-}
 
